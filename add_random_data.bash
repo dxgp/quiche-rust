2204f957c255335421eba595fd2206fae93f4599
dxgp <gunjanpayal@icloud.com>
2023-12-02 16:03:05 +0530

    random data

---
+++ add_random_data.bash
@@ -0,0 +1,8 @@
+dd if=/dev/zero of=rust-server/files/rand1.dat bs=1K count=5
+dd if=/dev/zero of=rust-server/files/rand2.dat bs=1K count=10
+dd if=/dev/zero of=rust-server/files/rand3.dat bs=1K count=100
+dd if=/dev/zero of=rust-server/files/rand4.dat bs=1K count=200
+dd if=/dev/zero of=rust-server/files/rand5.dat bs=1K count=500
+dd if=/dev/zero of=rust-server/files/rand6.dat bs=1M count=1
+dd if=/dev/zero of=rust-server/files/rand7.dat bs=1M count=10
+dd if=/dev/zero of=rust-server/files/rand8.dat bs=1M count=100
